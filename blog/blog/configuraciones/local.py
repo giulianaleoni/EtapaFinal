65a6f3cf47657469d03a45d83823d7417db35bf8
lucas <lucasalf1303@gmail.com>
2023-07-19 14:08:04 -0300

    Configuracion Inicial
    Complete la configuracion de las carpetas. El conector a la DB de mysql

--- blog/blog/configuraciones/local.py
+++ blog/blog/configuraciones/local.py
@@ -1,1 +1,12 @@
 from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyecto',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'3306',
+    }
+}
