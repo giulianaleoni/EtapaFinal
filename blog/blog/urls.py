763d6b4e7dc9523e0f4544f2e641b2e0602ea1a2
giulianaleoni <giuli_leoni@hotmail.com>
2023-07-19 11:55:53 -0300

    contacto

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index, name='index'),
     path('',include('apps.posts.urls')),
+    path('',include(apps.contacto.urls))
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
