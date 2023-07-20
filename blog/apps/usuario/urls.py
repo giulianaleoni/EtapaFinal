a131fb2745f82b6005e64a0496792bf321f199bb
giulianaleoni <giuli_leoni@hotmail.com>
2023-07-20 15:37:39 -0300

    Modelo usuario

---
+++ blog/apps/usuario/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+app_name= 'apps.usuario'
+
+urlpatterns = [
+    path('registrar/',views.RegistrarUsuario.as_view(),name='registar'),
+    path('login/',views.LoginUsuario.as_view(),name='login'),
+    path('logout',views.LogoutUsuario.as_view(),name='logout'),
+]
+
+
+
