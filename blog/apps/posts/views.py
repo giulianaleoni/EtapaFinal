ba1269e0e1c90b02191df5a4f92ea5f3613dcb94
Guidoolivera <guidoolivera1@gmail.com>
2023-07-20 20:42:15 -0300

    Creacion formulario, template, y actualizar post

--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from .forms import PostForm  # Asegúrate de crear este formulario
 from django.views.generic import ListView, DetailView
 
 
@@ -16,3 +17,14 @@ class PostDetailView(DetailView):
     context_object_name = "posts"
     pk_url_kwarg = "id"
     queryset = Post.objects.all()
+
+def actualizar_noticia(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts')
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'posts/post_form.html', {'form' : form})
