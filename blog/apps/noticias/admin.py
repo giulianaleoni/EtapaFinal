7d6d76ba2ff76035377fd9aa843bd2909515abd2
roni4lan <ronialan.es@gmail.com>
2023-07-21 11:59:52 -0300

    Agregado de lista de Posts, y vista de Posts
    Se agrgo la lista de lista de Posts, y vista de Posts

--- blog/apps/noticias/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Noticia
-# Register your models here.
-
-
-@admin.register(Noticia)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'contenido', 'fecha_publicacion')
--- blog/blog/configuraciones/local.py
+++ blog/blog/configuraciones/local.py
@@ -4,9 +4,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'proyecto',
-        'USER':'root',
-        'PASSWORD':'39618517c',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
