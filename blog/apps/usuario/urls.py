f4f6241dd16fd2834d51170065518b86d72b84c7
lucas <lucasalf1303@gmail.com>
2023-07-24 08:58:44 -0300

    login registro y logut
    Creacion de vistas y modificacion de vistas en base

--- blog/apps/usuario/urls.py
+++ blog/apps/usuario/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name= 'apps.usuario'
 
 urlpatterns = [
-    path('registrar/',views.RegistrarUsuario.as_view(),name='registar'),
+    path('registrar/',views.RegistrarUsuario.as_view(),name='registrar'),
     path('login/',views.LoginUsuario.as_view(),name='login'),
-    path('logout',views.LogoutUsuario.as_view(),name='logout'),
+    path('logout/',views.LogoutUsuario.as_view(),name='logout'),
     path('acercaDe/',views.acercaDe,name='about'),
 ]
 
