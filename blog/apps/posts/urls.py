5446eb083bcbc395963af4d55bf8a4c67b6e4ba1
lucas <lucasalf1303@gmail.com>
2023-07-28 16:57:55 -0300

    Nueva Funcionalidad de Los formularios de carga de Post

--- blog/apps/posts/urls.py
+++ blog/apps/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView,index,requestCategoria, postUser,agregarCategoria
+from .views import PostListView, PostDetailView,index,requestCategoria, postUser, AgregarPostView, AgregarCategoriaView
 from . import views
 
 app_name = 'apps.posts'
@@ -11,10 +11,10 @@ urlpatterns = [
     path('misposts/', postUser, name='postUser'),
     path('categoria/<int:id>', requestCategoria, name='categoria'),
     path('posts/editarPost/<int:id>/', views.editarPost, name='editar'),
-    path('post/crear/', views.agregarPost, name='agregar'),
     path('posts/eliminar/<int:id>/', views.eliminarPost, name='eliminar'),
-    path('categoria/', agregarCategoria, name='creacategoria'),
     path('posts/<int:post_id>/editar_comentario/<int:comentario_id>/', views.editar_comentario, name='editar_comentario'),
     path('posts/<int:post_id>/eliminar_comentario/<int:comentario_id>/', views.eliminar_comentario, name='eliminar_comentario'),
+    path('posts/agregar/cate', AgregarCategoriaView.as_view(), name='agregar_cate'),
+    path('posts/agregar/', AgregarPostView.as_view(), name='agregar_post'),
 ]
 
