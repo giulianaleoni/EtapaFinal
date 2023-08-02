bb344e8d192cded182795aab8b332ee93a75cf2d
lucas <lucasalf1303@gmail.com>
2023-08-02 09:54:28 -0300

    Pulido
    Se quito todas las importaciones innecesarias, eliminar las imagenes innecesarias , y las migraciones innecesarias y los pycache

--- blog/apps/posts/models.py
+++ blog/apps/posts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from apps.usuario.models import Usuario
-from django.shortcuts import render
 from django.conf import settings
 
 # Create your models here.
